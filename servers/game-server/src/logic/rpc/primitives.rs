d64896b9e4289dbf94399eebdc1badc0129e763d
xeon <xeon@xeondev.com>
2025-07-18 17:50:43 +0300

    Hi

---
+++ servers/game-server/src/logic/rpc/primitives.rs
@@ -0,0 +1,17 @@
+use fadia_engine::{
+    FNetworkGUID,
+    util::{PackedBitReadExt, PackedBitWriteExt},
+};
+
+use super::RpcArgument;
+
+impl RpcArgument for FNetworkGUID {
+    fn serialize(&self, w: &mut fadia_engine::util::OutBitWriter) -> std::io::Result<()> {
+        w.write_packed_int(self.0)?;
+        Ok(())
+    }
+
+    fn deserialize(r: &mut fadia_engine::util::InBitReader) -> std::io::Result<Self> {
+        Ok(Self(r.read_packed_int()?))
+    }
+}
